20c12c2ac770f15657e58abe2362c2a0ef6954df
Johan Larsson <jolars@posteo.com>
2025-05-15 22:09:46 +0200

    refactor: wrap test in testset macro

--- test/quadratic.jl
+++ test/quadratic.jl
@@ -4,34 +4,36 @@ using SparseArrays
 using Random
 
 # Simple design
-n = 3
-p = 2
+@testset "Simple design" begin
+  n = 3
+  p = 2
 
-x = [1.1 2.3; 0.5 1.5; 0.5 0.2]
-β = [1.0, 2.0]
-y = x * β
+  x = [1.1 2.3; 0.5 1.5; 0.5 0.2]
+  β = [1.0, 2.0]
+  y = x * β
 
-alpha = 1e-12
-lambda = [1.0, 1.0]
+  alpha = 1e-12
+  lambda = [1.0, 1.0]
 
-res = slope(x, y, α=alpha, λ=lambda)
+  res = slope(x, y, α=alpha, λ=lambda)
 
-@test res.β[1] ≈ β
+  @test res.β[1] ≈ β
 
-n = 20
-p = 2
-m = 1
+  n = 20
+  p = 2
+  m = 1
 
-# Dense and sparse agreement
-Random.seed!(58)
+  # Dense and sparse agreement
+  Random.seed!(58)
 
-x = SparseArrays.sprand(n, p, 0.5)
-y = rand(n)
+  x = SparseArrays.sprand(n, p, 0.5)
+  y = rand(n)
 
-path_length = 3
+  path_length = 3
 
-res_sparse = slope(x, y)
-res_dense = slope(Matrix(x), y)
+  res_sparse = slope(x, y)
+  res_dense = slope(Matrix(x), y)
 
-@test maximum(abs.(maximum.(res_dense.β - res_sparse.β))) < 1e-10
+  @test maximum(abs.(maximum.(res_dense.β - res_sparse.β))) < 1e-10
+end
 
