182e0611db9337b112ae80532fc3d3aa5e03e0fa
Johan Larsson <jolars@posteo.com>
2025-05-15 21:40:31 +0200

    refactor: reorganize test structure

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,40 +1,14 @@
 using SLOPE
 using Test
-using SparseArrays
-using Random
+using Aqua
 
-@testset "Simple design" begin
-  n = 3
-  p = 2
+@testset "SLOPE" begin
+  @testset "Quadratic" begin
+    include("quadratic.jl")
+  end
 
-  x = [1.1 2.3; 0.5 1.5; 0.5 0.2]
-  β = [1.0, 2.0]
-  y = x * β
-
-  alpha = 1e-12
-  lambda = [1.0, 1.0]
-
-  res = slope(x, y, α=alpha, λ=lambda)
-
-  @test res.β[1] ≈ β
+  @testset "Aqua" begin
+    Aqua.test_all(SLOPE)
+  end
 end
 
-@testset "Dense sparse agreement" begin
-  n = 20
-  p = 2
-  m = 1
-
-  Random.seed!(58)
-
-  x = SparseArrays.sprand(n, p, 0.5)
-  y = rand(n)
-
-  path_length = 3
-
-  res_sparse = slope(x, y)
-  res_dense = slope(Matrix(x), y)
-
-  @test maximum(abs.(maximum.(res_dense.β - res_sparse.β))) < 1e-10
-end
-
-include(joinpath(@__DIR__, "aqua.jl"))
