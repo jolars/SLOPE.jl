3966d87ff829286fd56d1a84566febd96315938d
Johan Larsson <johan@jolars.co>
2026-01-28 10:17:39 +0100

    refactor: fix conditional CI docs workflow

--- docs/make.jl
+++ docs/make.jl
@@ -3,20 +3,26 @@ using SLOPE
 
 is_ci = get(ENV, "CI", nothing) == "true"
 
-makedocs(
-    sitename = "SLOPE",
-    format = Documenter.HTML(
+makedocs_kwargs = Dict{Symbol, Any}(
+    :sitename => "SLOPE",
+    :format => Documenter.HTML(
         assets = ["assets/favicon.ico"],
         prettyurls = is_ci
     ),
-    modules = [SLOPE],
-    pages = [
+    :modules => [SLOPE],
+    :pages => [
         "Home" => "index.md",
         "Tutorial" => "tutorial.md",
         "API Reference" => "api.md",
-    ],
-    remotes = is_ci ? "https://github.com/jolars/SLOPE.jl" : nothing,
+    ]
 )
+
+# Disable source links locally to avoid NixOS git issues
+if !is_ci
+    makedocs_kwargs[:remotes] = nothing
+end
+
+makedocs(; makedocs_kwargs...)
 
 if is_ci
     deploydocs(
