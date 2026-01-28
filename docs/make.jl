70c3afdca05622e71ecca8af2e7bac5e9eacfb8f
Johan Larsson <johan@jolars.co>
2026-01-28 10:05:17 +0100

    refactor: setup local and CI versions of docs workflow

--- docs/make.jl
+++ docs/make.jl
@@ -1,19 +1,25 @@
 using Documenter
 using SLOPE
 
+is_ci = get(ENV, "CI", nothing) == "true"
+
 makedocs(
     sitename = "SLOPE",
     format = Documenter.HTML(
         assets = ["assets/favicon.ico"],
+        prettyurls = is_ci
     ),
     modules = [SLOPE],
     pages = [
         "Home" => "index.md",
         "Tutorial" => "tutorial.md",
         "API Reference" => "api.md",
-    ]
+    ],
+    remotes = is_ci ? "https://github.com/jolars/SLOPE.jl" : nothing,
 )
 
-deploydocs(
-    repo = "https://github.com/jolars/SLOPE.jl.git"
-)
+if is_ci
+    deploydocs(
+        repo = "github.com/jolars/SLOPE.jl.git",
+    )
+end
